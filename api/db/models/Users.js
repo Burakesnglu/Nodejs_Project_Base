f2674ff32856ea77452615e9455eb19272464ed0
Burak Esenoğlu <burakesngluuu@gmail.com>
2023-05-16 09:31:02 +0300

    auth

--- api/db/models/Users.js
+++ api/db/models/Users.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const is = require("is_js");
+const { PASS_LENGTH, HTTP_CODES } = require("../../config/Enum");
+const CustomError = require("../../lib/Error");
+const bcrypt = require("bcrypt-nodejs");
 
 const schema = mongoose.Schema({
     email: {
@@ -28,6 +32,17 @@ const schema = mongoose.Schema({
 });
 
 class Users extends mongoose.Model {
+
+    validPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+
+    static validateFieldsBeforeAuth(email, password) {
+        if (typeof password !== "string" || password.length < PASS_LENGTH || is.not.email(email))
+            throw new CustomError(HTTP_CODES.UNAUTHORIZED, "Validation Error", "Wrong email or password");
+
+        return null;
+    }
 
 }
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require("bcrypt-nodejs");
 const is = require("is_js");
+const jwt = require("jwt-simple");
 
 const Response = require('../lib/Response');
 const CustomError = require('../lib/Error');
@@ -9,6 +10,7 @@ const Users = require('../db/models/Users');
 const Enum = require('../config/Enum');
 const Roles = require('../db/models/Roles');
 const UserRoles = require('../db/models/UserRoles');
+const config = require('../config');
 
 router.get('/', async (req, res, next) => {
 
@@ -223,5 +225,39 @@ router.post('/register', async (req, res, next) => {
   }
 
 });
+
+router.post("/auth", async (req, res) => {
+  try {
+
+    let { email, password } = req.body;
+
+    Users.validateFieldsBeforeAuth(email, password);
+
+    let user = await Users.findOne({ email });
+
+    if (!user) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Wrong email or password");
+
+    if (!user.validPassword(password)) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Wrong email or password");
+
+    let payload = {
+      id: user._id,
+      exp: parseInt(Date.now() / 1000) * config.JWT.EXPIRE_TIME
+    }
+
+    let token = jwt.encode(payload, config.JWT.SECRET);
+
+    let userData = {
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name
+    }
+
+    res.json(Response.successResponse({ token, user: userData }));
+
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+})
 
 module.exports = router;
